a0c1810650ad4f972ec5bae8b23ee0ee0441b51e
R3zn1kov <ldlreznikov@gmail.com>
2024-02-26 22:54:17 +0100

    Added threading and send_email function

--- mailing.py
+++ mailing.py
@@ -1,2 +1,30 @@
-def send_email():
-    print("Email sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+# insert Gmail app password below
+password = ""
+sender = "ldlreznikov@gmail.com"
+receiver = "ldlreznikov@gmail.com"
+
+
+def send_email(image_path):
+
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey, we just saw a new customer!")
+
+    with open(image_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(sender, password, )
+    gmail.sendmail(sender, receiver, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/19.png")
--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+import glob
+import os
 import time
 from mailing import send_email
 import cv2
+import os
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
 first_frame = None
 status_list = []
+count = 1
+
+
+def clean_folder():
+    images = glob.glob("images/*.png")
+    for image in images:
+        os.remove(image)
+
+
 while True:
     status = 0
     check, frame = video.read()
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
 
@@ -22,19 +36,27 @@ while True:
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
-        if cv2.contourArea(contour)<5500:
+        if cv2.contourArea(contour) < 5500:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        rectangle = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
         if rectangle.any():
             status = 1
-            send_email()
+            cv2.imwrite(f"images/{count}.png", frame)
+            count = count + 1
+            all_images = glob.glob("images/*.png")
+            index = int(len(all_images) / 2)
+            image_with_object = all_images[index]
 
     status_list.append(status)
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        clean_thread = Thread(target=clean_folder)
+        email_thread.daemon = True
+
+        email_thread.start()
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -43,3 +65,6 @@ while True:
         break
 
 video.release()
+clean_folder()
+
+clean_thread.start()
